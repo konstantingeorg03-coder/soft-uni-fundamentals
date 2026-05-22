11273a80f3a88a5531e894321c18bddfa2b0478f
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-05-23 00:28:38 +0300

    Adding the solution to exercise num 7

--- dataAbdvariables/07. requiredReading.js
+++ dataAbdvariables/07. requiredReading.js
@@ -1,6 +1,8 @@
 function requiredReading(pages, pagesForOneHour, daysForReading){
     let readingHours = pages / pagesForOneHour;
+
     let totalHours = readingHours / daysForReading;
+
     console.log(totalHours);
 }
 requiredReading(432, 15, 4);
