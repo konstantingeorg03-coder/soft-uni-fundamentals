ce3b059057b8ac424dbeb5295094bba4c3b7ef2f
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-05-22 21:06:46 +0300

    The solution of exescise 6

--- dataAbdvariables/06. Gramophone.js
+++ dataAbdvariables/06. Gramophone.js
@@ -1,8 +1,8 @@
 function Gramophone(bandName, albumName, songAlbumName){
-    let songtime = (albumName.length * bandName.length) * songAlbumName.length / 2;
-    
-    let rotation = Math.ceil(songtime / 2.5);
+    let timeSong = (albumName.length * bandName.length) * songAlbumName.length / 2;
 
-    console.log(`The plate was rotated ${rotation} times.`);
+    let rotation = Math.ceil(timeSong / 2.5);
+
+    console.log(`The plate was rotated ${rotation} times`);
 }
 Gramophone('Black Sabbath', 'Paranoid', 'War Pigs');
