722930f45e463342a6e06fdfb44094ea6d2f96e5
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-05-24 22:40:38 +0300

    Adding the secong solution

--- dataAbdvariables/05.amazingNumbers.js
+++ dataAbdvariables/05.amazingNumbers.js
@@ -1,6 +1,6 @@
 function amazingNumbers(num){
     let sum = 0;
-    
+
     num = num.toString();
 
     for(let i = 0; i < num.length; i++){
@@ -8,6 +8,6 @@ function amazingNumbers(num){
     }
     let result = sum.toString().includes('9');
 
-    console.log(`${num} Amazing? ${result ? 'True' : 'False'}`);
-}    
-amazingNumbers(1233);
+    console.log(`${sum} Amazing? ${result ? 'True' : 'False'}`);
+}
+amazingNumbers(1233);``
