6ccefcc9e111e3da1ec77213d5f94076ada3d2fe
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-06-03 21:29:50 +0300

    adding the solution of ex 5

--- basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/05. simpleCalculator.js
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/05. simpleCalculator.js
@@ -2,14 +2,22 @@ function calculator(first, second, operator){
     let operation;
 
     if(operator === 'multiply'){
-        operation = (x, y) => (x * y);
-    }else if(operator === 'divide'){
-        operation = (x, y) => (x / y);
-    }else if(operator === 'add'){
-        operation = (x, y) => (x + y);
-    }else if(operator === 'subtract'){
-        operation = (x, y) => (x - y);
+        operation = (x, y) => x * y;
+    }
+
+    if(operator === 'divide'){
+        operation = (x, y) => x / y;
+    }
+
+    if(operator === 'add'){
+        operation = (x, y) => x + y;
+    }
+
+    if(operator === 'subtract'){
+        operation = (x, y) => x - y;
     }
 
     return operation(first, second);
 }
+
+console.log(calculator(5, 5, 'multiply'));
