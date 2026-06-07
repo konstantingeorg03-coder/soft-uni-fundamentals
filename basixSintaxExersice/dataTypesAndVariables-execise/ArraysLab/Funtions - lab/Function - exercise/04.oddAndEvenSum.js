4c4214f338e9c746d5b09256881276c4d58eb78a
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-06-07 20:31:15 +0300

    add the solution 2 for exercise 4

--- basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/04.oddAndEvenSum.js
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/04.oddAndEvenSum.js
@@ -1,18 +1,18 @@
 function oddAndEvenSum(num){
-    let oddSum = 0;
-    let evenSum = 0;
+   let oddSum = 0;
+   let evenSum = 0;
 
-    let numAsString = String(num);
+   num = String(num);
 
-    for(let char of numAsString){
-        let digitNum = Number(char);
-
-        if(char % 2 === 0){
-            evenSum += digitNum;
-        }else{
-            oddSum += digitNum;
-        }
+   for(let number of num){
+    let currentNum = Number(number);
+    if(number % 2 === 0){
+        evenSum += currentNum;
+    }else{
+        oddSum += currentNum;
     }
-    console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
+   }
+   
+   console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
 }
 oddAndEvenSum(1000435);
