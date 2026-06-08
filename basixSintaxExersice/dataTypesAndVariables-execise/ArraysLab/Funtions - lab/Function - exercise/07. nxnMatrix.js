064a3c295174bbc741afbc1e76918374fa8888f0
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-06-08 23:35:30 +0300

    add the solution 2 for exercise 7

--- basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/07. nxnMatrix.js
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/07. nxnMatrix.js
@@ -1,14 +1,15 @@
 function nxnMatrix(n){
-   for(let i = 1; i <= n; i++){
-    console.log(createRow(n));
-   } 
-
-   function createRow(n){
-    let row = "";
-    for(let j = 1; j <= n; j++){
-        row += n + ' ';
+    for(let i = 1; i <= n; i++){
+        console.log(createRow(n));
     }
-    return row;
-   }
+
+    function createRow(n){
+        let row = '';
+
+        for(let j = 1; j <= n; j++){
+            row += n + ' ';
+        }
+        return row;
+    }
 }
 nxnMatrix(7);
