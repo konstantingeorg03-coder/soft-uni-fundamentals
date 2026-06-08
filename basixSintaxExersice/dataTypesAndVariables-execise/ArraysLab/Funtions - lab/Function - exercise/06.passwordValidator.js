cee90e9cdcec350b7c2ddb0008a5c528283e61d4
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-06-08 22:06:58 +0300

    add the solution num 2 for exercise 6

--- basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/06.passwordValidator.js
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/06.passwordValidator.js
@@ -1,37 +1,38 @@
 function passwordValidator(password) {
-    let isLengthValid = password.length >= 6 && password.length <= 10;
-    let lettersAndDigits = true;
+    let isLengtValid = password.length >= 6 && password.length <= 10;
+    let isLettersAndDigits = true;
     let digitsCount = 0;
 
-    for (let i = 0; i < password.length; i++) {
+    for(let i = 0; i < password.length; i++){
         let ascii = password[i].charCodeAt(0);
 
         let isDigit = ascii >= 48 && ascii <= 57;
         let isUpperCase = ascii >= 65 && ascii <= 90;
         let isLowerCase = ascii >= 97 && ascii <= 122;
 
-        if (!isDigit && !isUpperCase && !isLowerCase) {
-            lettersAndDigits = false;
+        if(!isDigit && !isUpperCase && !isLowerCase){
+            isLettersAndDigits = false;
         }
 
-        if (isDigit) {
+        if(isDigit){
             digitsCount++;
         }
     }
 
-    if (isLengthValid && lettersAndDigits && digitsCount >= 2) {
+    if(isLengtValid && isLettersAndDigits && digitsCount){
         console.log('Password is valid');
     }
 
-    if (!isLengthValid) {
+    if(!isLengtValid){
         console.log('Password must be between 6 and 10 characters');
     }
 
-    if (!lettersAndDigits) {
-        console.log('Password must consist only of letters and digits');
+    if(!isLettersAndDigits){
+        console.log('Password must be consist only of letters and digits')
     }
 
-    if (digitsCount < 2) {
+    if(!digitsCount){
         console.log('Password must have at least 2 digits');
     }
 }
+passwordValidator('logIn');
