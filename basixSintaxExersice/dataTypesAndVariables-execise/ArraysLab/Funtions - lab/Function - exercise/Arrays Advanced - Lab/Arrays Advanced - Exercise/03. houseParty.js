2242fa52d92b2954a35e3024bbbe6ecc5b1bb54b
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-06-13 02:31:35 +0300

    add the solution for ex 3 for arrays advanced exercise

---
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/03. houseParty.js
@@ -0,0 +1,31 @@
+function houseParty(arr){
+    let guests = [];
+
+    for(let command of arr){
+        if(command.includes('is going')){
+            let tokens = command.split(' ');
+            let name = tokens[0];
+
+            if(!guests.includes(name)){
+                guests.push(name);
+            }else{
+                console.log(`${name} is already in the list!`);
+            }
+        }else if(command.includes('is not going')){
+            let tokens = command.split(' ');
+            let name = tokens[0];
+
+            if(guests.includes(name)){
+                let index = guests.indexOf(name);
+                guests.splice(index, 1);
+            }else{
+                console.log(`${name} is not in the list!`);
+            }
+        }
+    }
+    console.log(guests.join('\n'));
+}
+houseParty(['Allie is going!',
+'George is going!',
+'John is not going!',
+'George is not going!']);
