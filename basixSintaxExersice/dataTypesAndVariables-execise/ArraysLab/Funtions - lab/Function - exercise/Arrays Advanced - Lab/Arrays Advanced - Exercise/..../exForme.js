2680574a3f3823c7591270485bcf85380a988d37
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-06-28 14:43:45 +0300

    add the exercies 8 from objects and classes - exercise

--- basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../exForme.js
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../exForme.js
@@ -1,17 +1,46 @@
 function solve(arr){
-    for(let names of arr){
-        let [name, longitude, latitude] = names.split(' | ');
-        latitude = Number(latitude);
-        longitude = Number(longitude);
+    let newArr = [];
 
-        let namesObj = {
-            name: name,
-            latitude: latitude.toFixed(2),
-            longitude: longitude.toFixed(2)
-        };
+    for(let command of arr){
+        if(command.includes('addMovie')){
+            let current = command.split('addMovie ');
+            let movie = current[1];
 
-        console.log(namesObj);
+            newArr.push({ name: movie});
+        }else if(command.includes('directedBy')){
+            let current = command.split(' directedBy ');
+            let [nameMovie, movieDirector] = current;
+
+            let movieFound = newArr.find(movie => movie.name === nameMovie);
+
+            if(movieFound){
+                movieFound.director = movieDirector;
+            }
+        }else if(command.includes('onDate')){
+            let current = command.split(' onDate ');
+            let [nameMovie, movieDate] = current;
+
+            let movieFound = newArr.find(movie => movie.name === nameMovie);
+
+            if(movieFound){
+                movieFound.date = movieDate;
+            }
+        }
+    }
+
+    for(let data of newArr){
+        if(data.name && data.director && data.date){
+            console.log(JSON.stringify(data))
+        }
     }
 }
-solve(['Sofia | 42.696552 | 23.32601',
-'Beijing | 39.913818 | 116.363625']);
+solve([
+'addMovie Fast and Furious',
+'addMovie Godfather',
+'Inception directedBy Christopher Nolan',
+'Godfather directedBy Francis Ford Coppola',
+'Godfather onDate 29.07.2018',
+'Fast and Furious onDate 30.07.2018',
+'Batman onDate 01.08.2018',
+'Fast and Furious directedBy Rob Cohen'
+]);
