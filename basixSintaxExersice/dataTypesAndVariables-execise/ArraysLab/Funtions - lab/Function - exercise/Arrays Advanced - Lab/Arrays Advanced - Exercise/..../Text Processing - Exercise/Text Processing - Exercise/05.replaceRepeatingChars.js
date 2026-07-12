6870a5fd2e1681a6c8b4a7b3a199372d735c5dd3
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-07-12 17:41:40 +0300

    add rhe exercise 5 from Text Processing - Exercise

---
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../Text Processing - Exercise/Text Processing - Exercise/05.replaceRepeatingChars.js
@@ -0,0 +1,12 @@
+function solve(str){
+    let result = '';
+
+    for(let i = 0; i < str.length; i++){
+        if(str[i] !== str[i + 1]){
+            result += (str[i]);
+        }
+    }
+
+    console.log(result);
+}
+solve('aaaaabbbbbcdddeeeedssaa');
