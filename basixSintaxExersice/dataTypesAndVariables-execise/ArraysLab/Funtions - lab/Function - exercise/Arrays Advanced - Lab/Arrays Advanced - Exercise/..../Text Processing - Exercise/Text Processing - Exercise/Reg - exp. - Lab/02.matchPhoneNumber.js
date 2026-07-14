510822453bfe5646b90cde0f5fe80c15a93102ba
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-07-14 19:20:37 +0300

    add the exercise 2 from Reg - exp. - Lab

---
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../Text Processing - Exercise/Text Processing - Exercise/Reg - exp. - Lab/02.matchPhoneNumber.js
@@ -0,0 +1,14 @@
+function solve(input){
+    let pattern = /\+359([ -])2\1\d{3}\1\d{4}\b/g;
+
+    let matches = input[0].matchAll(pattern);
+
+    let output = []
+
+    for(let match of matches){
+        output.push(match[0]);
+    }
+
+    console.log(output.join(', '));
+}
+solve(['+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222']);
