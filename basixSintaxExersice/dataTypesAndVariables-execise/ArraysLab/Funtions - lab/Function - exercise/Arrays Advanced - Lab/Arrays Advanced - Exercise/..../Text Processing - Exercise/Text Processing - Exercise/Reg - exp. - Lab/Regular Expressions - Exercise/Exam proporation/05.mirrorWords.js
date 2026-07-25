d4be548d1d1f043806fdca5610419d60fedfb702
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-07-25 16:02:57 +0300

    add the exercise 5 from Exam proporation

--- basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../Text Processing - Exercise/Text Processing - Exercise/Reg - exp. - Lab/Regular Expressions - Exercise/Exam proporation/05.mirrorWords.js
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../Text Processing - Exercise/Text Processing - Exercise/Reg - exp. - Lab/Regular Expressions - Exercise/Exam proporation/05.mirrorWords.js
@@ -35,6 +35,4 @@ function solve(input){
         console.log(mirrorWords.join(', '));
     }
 }
-solve([
-    '@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r'
-]);
+solve(['@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r']);
