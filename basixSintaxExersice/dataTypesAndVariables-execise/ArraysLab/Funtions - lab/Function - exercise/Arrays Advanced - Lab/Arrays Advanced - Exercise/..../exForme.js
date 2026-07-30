20a00e697bf41a471453e0446376f8f461b55ad5
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-07-30 22:14:43 +0300

    add the solution number 2 for exercise 13 from Exam Proporation

--- basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../exForme.js
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../exForme.js
@@ -1,32 +1,67 @@
 function solve(arr){
-    let text = arr.shift();
+    let activityKey = arr.shift();
 
-    let pattern = /(?<start>[::|\*\*])(?<emojiName>[A-Z][a-z]{2,})\k<start>/g;
+    let command = arr.shift();
 
-    let digits = /\d/;
+    while(command !== 'Generate'){
+        let tokens = command.split('>>>');
 
-    let match = text.match(digits);
+        let action = tokens[0];
 
-    let coolThreShold = 1;
+        if(action === 'Contains'){
+            let substr = tokens[1];
 
-    for(let digit of match){
-        coolThreShold *= Number(digit);
+            if(activityKey.includes(substr)){
+                console.log(`${activityKey} contains ${substr}`);
+            }else{
+                console.log('Substring not found!');
+            }
+        }else if(action === 'Flip'){
+            let type = tokens[1];
+
+            let startIdx = Number(tokens[2]);
+
+            let endIdx = Number(tokens[3]);
+
+            let part = activityKey.slice(startIdx, endIdx);
+
+            if(type === 'Upper'){
+                part = part.toUpperCase();
+
+            }else if(type === 'Lower'){
+                part = part.toLowerCase();
+            }
+
+            let firstPart = activityKey.slice(0, startIdx);
+
+            let secondPart = activityKey.slice(endIdx);
+
+            activityKey = firstPart + part + secondPart;
+
+            console.log(activityKey);
+        }else if(action === 'Slice'){
+            let startIdx = Number(tokens[1]);
+
+            let endIdx = Number(tokens[2]);
+
+            let firstPart = activityKey.slice(0, startIdx);
+
+            let secondPart = activityKey.slice(endIdx);
+
+            activityKey = firstPart + secondPart;
+
+            console.log(activityKey);
+        }
+
+        command = arr.shift();
     }
 
-    console.log(`Cool threshold: ${coolThreshold}`);
-
-    let patternEmoji = text.matchAll(pattern);
-
-    let emojies = [];
-
-    let ascii = 0;
-
-    for(let letter of patternEmoji){
-        ascii = letter.charCodeAt();
-    }
-
-    if(ascii >= coolThreShold){
-        
-    }
+    console.log(`Your activation key is: ${activityKey}`);
 }
-solve(['In the Sofia Zoo there are 311 animals in total! ::Smiley:: This includes 3 **Tigers**, 1 ::Elephant:, 12 **Monk3ys**, a **Gorilla::, 5 ::fox:es: and 21 different types of :Snak::Es::. ::Mooning:: **Shy**']);
+solve(['abcdefghijklmnopqrstuvwxyz',
+'Slice>>>2>>>6',
+'Flip>>>Upper>>>3>>>14',
+'Flip>>>Lower>>>5>>>7',
+'Contains>>>def',
+'Contains>>>deF',
+'Generate'])
