8c45a52d80b1bcc151a541f2dc0268ad271f12a1
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-08-21 21:41:45 +0300

    add the solution 2 for exercise Secret Chat

--- basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../exForme.js
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../exForme.js
@@ -1,4 +1,60 @@
 function solve(arr){
-    
+    let message = arr.shift();
+
+    let command = arr.shift();
+
+    while(command !== 'Reveal'){
+        let tokens = command.split(':|:');
+
+        let action = tokens[0];
+
+        if(action === 'InsertSpace'){
+            let idx = Number(tokens[1]);
+
+            let first = message.substring(0, idx);
+
+            let second = message.substring(idx);
+
+            message = first + ' ' + second;
+
+            console.log(message);
+
+        }else if(action === 'Reverse'){
+            let substr = tokens[1];
+
+            if(message.includes(substr)){
+                let index = message.indexOf(substr);
+
+                let first = message.substring(0, index);
+
+                let second = message.substring(index + substr.length);
+            
+                message = first + second;
+
+                substr = substr.split('').reverse().join('');
+
+                message += substr;
+            }
+
+            console.log(message);
+
+        }else if(action === 'ChangeAll'){
+            let substr = tokens[1];
+
+            let replacement = tokens[2];
+
+            message = message.replaceAll(substr, replacement);
+
+            console.log(message);
+        }
+
+        command = arr.shift();
+    }
+
+    console.log(`You have a new text message: ${message}`);
 }
-solve(['=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=']);
+solve(['heVVodar!gniV',
+'ChangeAll:|:V:|:l',
+'Reverse:|:!gnil',
+'InsertSpace:|:5',
+'Reveal']);
