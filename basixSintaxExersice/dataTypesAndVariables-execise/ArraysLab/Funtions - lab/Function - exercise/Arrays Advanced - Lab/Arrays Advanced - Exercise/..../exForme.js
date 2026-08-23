3ecb8bf1f8cdddaca624c25facfba2644b310c46
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-08-23 12:31:30 +0300

    add the solution 2 for exercise Pirates

--- basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../exForme.js
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../exForme.js
@@ -1,61 +1,88 @@
 function solve(arr){
-    let str = arr.shift();
+    let cityStats = {};
+
+    let cities = arr.shift();
+
+    while(cities !== 'Sail'){
+        let tokens = cities.split('||');
+
+        let city = tokens[0];
+
+        let population = Number(tokens[1]);
+
+        let gold = Number(tokens[2]);
+
+        if(city in cityStats){
+            cityStats[city].population += population;
+
+            cityStats[city].gold += gold;
+        }else{
+            cityStats[city] = {population, gold};
+        }
+
+        cities = arr.shift();
+    }
 
     let command = arr.shift();
 
-    while(command !== 'Done'){
-        let tokens = command.split(' ');
+    while(command !== 'End'){
+        let tokens = command.split('=>');
 
         let action = tokens[0];
 
-        if(action === 'TakeOdd'){
-            let newStr = '';
+        if(action === 'Plunder'){
+            let city = tokens[1];
 
-            for(let i = 0; i < str.length; i++){
-                if(i % 2 === 1){
-                    newStr += str[i];
-                }
+            let people = Number(tokens[2]);
+
+            let gold = Number(tokens[3]);
+
+            cityStats[city].population -= people;
+
+            cityStats[city].gold -= gold;
+
+            console.log(`${city} plundered! ${gold} gold stolen, ${people} citizens killed.`);
+
+            if(cityStats[city].gold <= 0 || cityStats[city].population <= 0){
+                delete cityStats[city];
+
+                console.log(`${city} has been wiped off the map!`);
             }
+        }else if(action === 'Prosper'){
+            let city = tokens[1];
 
-            str = newStr;
+            let gold = Number(tokens[2]);
 
-            console.log(str);
+            if(gold < 0){
+                console.log('Gold added cannot be a negative number!');
+            }else if(gold >= 0){
+                cityStats[city].gold += gold;
 
-        }else if(action === 'Cut'){
-            let idx = Number(tokens[1]);
-
-            let length = Number(tokens[2]);
-
-            let first = str.substring(0, idx);
-
-            let second = str.substring(idx + length);
-
-            str = first + second;
-
-            console.log(str);
-
-        }else if(action === 'Substitute'){
-            let substr = tokens[1];
-
-            let substitute = tokens[2];
-
-            if(str.includes(substr)){
-                str = str.replaceAll(substr, substitute);
-
-                console.log(str);
-            }else{
-                console.log('Nothing to replace!');
+                console.log(`${gold} gold added to the city treasury. ${city} now has ${cityStats[city].gold} gold.`);
             }
         }
 
         command = arr.shift();
     }
 
-    console.log(`Your password is: ${str}`);
+    let citiesCount = Object.keys(cityStats);
+
+    if(citiesCount.length > 0){
+        console.log(`Ahoy, Captain! There are ${citiesCount.length} wealthy settlements to go to:`);
+
+        for(let city in cityStats){
+            let population = cityStats[city].population;
+
+            let gold = cityStats[city].gold;
+
+            console.log(`${city} -> Population: ${population} citizens, Gold: ${gold} kg`);
+        }
+    }
 }
-solve(['Siiceercaroetavm!:?:ahsott.:i:nstupmomceqr', 
-'TakeOdd',
-'Cut 15 3',
-'Substitute :: -',
-'Substitute | ^',
-'Done']);
+solve(['Tortuga||345000||1250',
+'Santo Domingo||240000||630',
+'Havana||410000||1100',
+'Sail',
+'Plunder=>Tortuga=>75000=>380',
+'Prosper=>Santo Domingo=>180',
+'End']);
