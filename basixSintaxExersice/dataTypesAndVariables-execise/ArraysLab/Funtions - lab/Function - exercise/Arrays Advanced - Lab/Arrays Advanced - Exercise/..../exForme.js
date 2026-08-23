dd78a77386f8228b727ae69502d9a7aae5e45b93
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-08-23 20:20:15 +0300

    add the solution 2 for exercise the lift

--- basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../exForme.js
+++ basixSintaxExersice/dataTypesAndVariables-execise/ArraysLab/Funtions - lab/Function - exercise/Arrays Advanced - Lab/Arrays Advanced - Exercise/..../exForme.js
@@ -1,88 +1,37 @@
-function solve(arr){
-    let cityStats = {};
+function solve(arr) {
+    let waitingPeople = Number(arr.shift());
+    let wagons = arr.shift().split(' ').map(Number);
 
-    let cities = arr.shift();
+    for (let i = 0; i < wagons.length; i++) {
+        let currentWagon = wagons[i];
+        let freeSpaces = 4 - currentWagon;
 
-    while(cities !== 'Sail'){
-        let tokens = cities.split('||');
-
-        let city = tokens[0];
-
-        let population = Number(tokens[1]);
-
-        let gold = Number(tokens[2]);
-
-        if(city in cityStats){
-            cityStats[city].population += population;
-
-            cityStats[city].gold += gold;
-        }else{
-            cityStats[city] = {population, gold};
-        }
-
-        cities = arr.shift();
-    }
-
-    let command = arr.shift();
-
-    while(command !== 'End'){
-        let tokens = command.split('=>');
-
-        let action = tokens[0];
-
-        if(action === 'Plunder'){
-            let city = tokens[1];
-
-            let people = Number(tokens[2]);
-
-            let gold = Number(tokens[3]);
-
-            cityStats[city].population -= people;
-
-            cityStats[city].gold -= gold;
-
-            console.log(`${city} plundered! ${gold} gold stolen, ${people} citizens killed.`);
-
-            if(cityStats[city].gold <= 0 || cityStats[city].population <= 0){
-                delete cityStats[city];
-
-                console.log(`${city} has been wiped off the map!`);
-            }
-        }else if(action === 'Prosper'){
-            let city = tokens[1];
-
-            let gold = Number(tokens[2]);
-
-            if(gold < 0){
-                console.log('Gold added cannot be a negative number!');
-            }else if(gold >= 0){
-                cityStats[city].gold += gold;
-
-                console.log(`${gold} gold added to the city treasury. ${city} now has ${cityStats[city].gold} gold.`);
-            }
-        }
-
-        command = arr.shift();
-    }
-
-    let citiesCount = Object.keys(cityStats);
-
-    if(citiesCount.length > 0){
-        console.log(`Ahoy, Captain! There are ${citiesCount.length} wealthy settlements to go to:`);
-
-        for(let city in cityStats){
-            let population = cityStats[city].population;
-
-            let gold = cityStats[city].gold;
-
-            console.log(`${city} -> Population: ${population} citizens, Gold: ${gold} kg`);
+        if (waitingPeople > freeSpaces) {
+            wagons[i] = 4;
+            waitingPeople -= freeSpaces;
+        } else {
+            wagons[i] += waitingPeople;
+            waitingPeople = 0;
+            break;
         }
     }
+
+    let hasEmptySpots = false;
+
+    for (let i = 0; i < wagons.length; i++) {
+        if (wagons[i] < 4) {
+            hasEmptySpots = true;
+            break;
+        }
+    }
+
+    if (waitingPeople > 0) {
+        console.log(`There isn't enough space! ${waitingPeople} people in a queue!`);
+    } else if (hasEmptySpots) {
+        console.log('The lift has empty spots!');
+    }
+
+    console.log(wagons.join(' '));
 }
-solve(['Tortuga||345000||1250',
-'Santo Domingo||240000||630',
-'Havana||410000||1100',
-'Sail',
-'Plunder=>Tortuga=>75000=>380',
-'Prosper=>Santo Domingo=>180',
-'End']);
+solve(['15',
+'0 0 0 0']);
